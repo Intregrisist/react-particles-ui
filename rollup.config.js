d7a1a718bb77ddc24fe36d9d660cbd2e2d2d506d
Daniel Flores <dflores009@gmail.com>
2024-02-03 12:29:08 -0800

    ci: cleanup dist folder before build

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
 import postcssPresetEnv from 'postcss-preset-env';
+import del from 'rollup-plugin-delete';
+import path from 'path';
 
 import pkg from './package.json';
 
@@ -36,9 +38,16 @@ export default {
     },
   ],
   plugins: [
+    del({
+      targets: 'dist/*',
+    }),
     postcss({
-      extract: true,
-      plugins: [postcssPresetEnv(/* pluginOptions */)],
+      extract: path.resolve('dist/styles.css'),
+      plugins: [
+        postcssPresetEnv({
+          stage: 2,
+        }),
+      ],
     }),
     typescript({
       tsconfig: 'tsconfig.build.json',
