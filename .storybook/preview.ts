d2bde3316ccfbb10ac9534416e4c5afb1fc975b1
Daniel Flores <dflores009@gmail.com>
2024-03-19 18:42:44 -0700

    chore: upgrade to Storybook 8

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,7 +4,7 @@ import '../dist/styles.css';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
+    // actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
